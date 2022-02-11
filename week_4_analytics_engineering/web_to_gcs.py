629724297f0aecb9556c2bb4d2f72bfe23037cab
sebastian2296 <sebastian-2296@hotmail.com>
2022-02-11 14:20:17 -0500

    modified web_to_gcs.py

--- week_4_analytics_engineering/web_to_gcs.py
+++ week_4_analytics_engineering/web_to_gcs.py
@@ -1,8 +1,10 @@
-import io
 import os
-import requests
 import pandas as pd
+import pyarrow.csv as pv
+import pyarrow.parquet as pq
+import pyarrow as pa
 from google.cloud import storage
+import logging
 
 """
 Pre-reqs: 
@@ -15,15 +17,48 @@ Pre-reqs:
 init_url = 'https://nyc-tlc.s3.amazonaws.com/trip+data/'
 BUCKET = os.environ.get("GCP_GCS_BUCKET", "dtc_data_lake_norse-bond-337916")
 
+table_schema = pa.schema(
+    [
+        ('VendorID',pa.string()),
+        ('lpep_pickup_datetime',pa.timestamp('s')),
+        ('lpep_dropoff_datetime',pa.timestamp('s')),
+        ('store_and_fwd_flag',pa.string()),
+        ('RatecodeID',pa.int64()),
+        ('PULocationID',pa.int64()),
+        ('DOLocationID',pa.int64()),
+        ('passenger_count',pa.int64()),
+        ('trip_distance',pa.float64()),
+        ('fare_amount',pa.float64()),
+        ('extra',pa.float64()),
+        ('mta_tax',pa.float64()),
+        ('tip_amount',pa.float64()),
+        ('tolls_amount',pa.float64()),
+        ('ehail_fee',pa.float64()),
+        ('improvement_surcharge',pa.float64()),
+        ('total_amount',pa.float64()),
+        ('payment_type',pa.int64()),
+        ('trip_type',pa.int64()),
+        ('congestion_surcharge',pa.float64()),
+    ]
+)
+
+def format_to_parquet(src_file):
+    if not src_file.endswith('.csv'):
+        logging.error("Can only accept source files in CSV format, for the moment")
+        return
+    table = pv.read_csv(src_file)
+    table = table.cast(table_schema)
+    pq.write_table(table, src_file.replace('.csv', '.parquet'))
+
 
 def upload_to_gcs(bucket, object_name, local_file):
     """
     Ref: https://cloud.google.com/storage/docs/uploading-objects#storage-upload-object-python
     """
-    # WORKAROUND to prevent timeout for files > 6 MB on 800 kbps upload speed.
-    # (Ref: https://github.com/googleapis/python-storage/issues/74)
-    storage.blob._MAX_MULTIPART_SIZE = 5 * 1024 * 1024  # 5 MB
-    storage.blob._DEFAULT_CHUNKSIZE = 5 * 1024 * 1024  # 5 MB
+    # # WORKAROUND to prevent timeout for files > 6 MB on 800 kbps upload speed.
+    # # (Ref: https://github.com/googleapis/python-storage/issues/74)
+    # storage.blob._MAX_MULTIPART_SIZE = 5 * 1024 * 1024  # 5 MB
+    # storage.blob._DEFAULT_CHUNKSIZE = 5 * 1024 * 1024  # 5 MB
 
     client = storage.Client()
     bucket = client.bucket(bucket)
@@ -33,20 +68,20 @@ def upload_to_gcs(bucket, object_name, local_file):
 
 def web_to_gcs(year, service):
     for i in range(13):
-        month = '0'+str(i+1)
-        month = month[-2:]
-        file_name = service + '_tripdata_' + year + '-' + month + '.csv'
-        request_url = init_url + file_name
-        r = requests.get(request_url)
-        pd.DataFrame(io.StringIO(r.text)).to_csv(file_name)
-        print(f"Local: {file_name}")
-        df = pd.read_csv(file_name)
-        file_name = file_name.replace('.csv', '.parquet')
-        df.to_parquet(file_name, engine='pyarrow')
-        print(f"Parquet: {file_name}")
-        upload_to_gcs(BUCKET, f"{service}/{file_name}", file_name)
-        print(f"GCS: {service}/{file_name}")
+        if i != 12:
+            month = '0'+str(i+1)
+            month = month[-2:]
+            file_name = service + '_tripdata_' + year + '-' + month + '.csv'
+            request_url = init_url + file_name
+            df = pd.read_csv(request_url)
+            df.to_csv(file_name, index = False)
+            print(f"Local: {file_name}")
+            parquetized = format_to_parquet(file_name)
+            file_name = file_name.replace('.csv', '.parquet')
+            print(f"Parquet: {file_name}")
+            upload_to_gcs(BUCKET, f"{service}/{file_name}", file_name)
+            print(f"GCS: {service}/{file_name}")
 
 
-web_to_gcs('2019', 'yellow')
-web_to_gcs('2020', 'yellow')
+web_to_gcs('2019', 'green')
+web_to_gcs('2020', 'green')
